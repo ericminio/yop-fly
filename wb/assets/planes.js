368f40a3bac89d5a28079c0d90c6c5dd0cbb9ac5
ericminio <eric.mignot@gmail.com>
2020-05-31 16:04:37 -0700

    vfc - 2020-01-28

--- wb/assets/planes.js
+++ wb/assets/planes.js
@@ -2,20 +2,14 @@ var planes = [
     {
         name: 'CGBMO',
         type: '172s',
-        weight: 1657.65,
-        moment: 67045.35
-    },
-    {
-        name: 'CFOJL',
-        type: '152',
-        weight: 1138,
-        moment: 34000
+        weight: 1659.25,
+        moment: 67211.94
     },
     {
         name: 'CGJZB',
         type: '172s',
-        weight: 1647.3,
-        moment: 66200.94
+        weight: 1650.5,
+        moment: 66371.89
     },
     {
         name: 'CGKMY',
@@ -26,37 +20,43 @@ var planes = [
     {
         name: 'CGSDZ',
         type: '172s',
-        weight: 1697.22,
-        moment: 69830.92
+        weight: 1700.43,
+        moment: 70001.87
     },
     {
         name: 'CGTDK',
         type: '172s',
-        weight: 1698.4,
-        moment: 69931.02
+        weight: 1701.61,
+        moment: 70701.97
     },
     {
         name: 'CGTEX',
         type: '172s',
-        weight: 1682.3,
-        moment: 67814.81
+        weight: 1682.91,
+        moment: 67955.49
     },
     {
         name: 'CGTLF',
         type: '172s',
-        weight: 1705.88,
-        moment: 70718.75
+        weight: 1697.34,
+        moment: 70584.4
     },
     {
         name: 'CGWLY',
         type: '172s',
-        weight: 1668.7,
-        moment: 67822.05
+        weight: 1680.95,
+        moment: 67933.00
     },
     {
         name: 'CGXTZ',
         type: '172s',
-        weight: 1697.2,
-        moment: 69824.81
+        weight: 1697.81,
+        moment: 69964.3
+    },
+    {
+        name: 'CFOJL',
+        type: '152',
+        weight: 1138,
+        moment: 34000
     }
 ];
